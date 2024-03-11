749294d960d672c03ba600fe985cbf7a84855258
Constanza Pérez <constanza.vp.o@gmail.com>
2024-03-10 21:26:23 -0300

    validaciones

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -3,19 +3,45 @@ import Form from 'react-bootstrap/Form';
 import { useState } from 'react';
 
 
-const Formulario = () => {
+const Formulario = ({setAlert}) => {
   const [nombre, setNombre] = useState("");
   const [ email, setEmail] = useState("");
   const [ password, setPassword] = useState("");
   const [ confirmPassword, setConfirmPassword] =useState("");
 
-  console.log(nombre)
-  console.log(email)
-  console.log(password)
-  console.log(confirmPassword)
+  const validarDatos = (e) => {
+
+    e.preventDefault()
+
+    if(nombre==="" || email==="" || password==="" || confirmPassword===""){
+      setAlert({
+        error: true,
+        msj: "Completa los campos vacios",
+        color: "danger"
+      })
+      return
+    }
+
+    if(password !== confirmPassword){
+      setAlert({
+        error: true,
+        msj: "Las contraseñas no coinciden",
+        color: "danger"
+      })
+      return
+    }
+
+    setAlert({error:false, msj: "Cuenta creada exitosamente!", color:"success"})
+    setNombre("")
+    setEmail("")
+    setPassword("")
+    setConfirmPassword("")
+  }
+
+
 
   return (
-    <Form className="formulario">
+    <Form className="formulario" onSubmit={validarDatos}>
       <Form.Group className="mb-3"  >
         <Form.Control
         type="text"
--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -1,8 +1,12 @@
+import { useState } from 'react'
 import Formulario from './Formulario'
 import SocialButton from './SocialButton'
 
 
 function Registro() {
+
+  const[alert, setAlert]= useState({error: "", msj: "", color: ""})
+
   return (
     <div className='card'>
       <h2>Crear una cuenta</h2>
@@ -13,7 +17,8 @@ function Registro() {
       </div>
 
       <p className='parrafo'>O usa tu email para registrarte</p>
-      <Formulario />
+      <Formulario setAlert={setAlert} />
+      {alert.msj && <alert color={alert.color}>{alert.msj}</alert>}
     </div>
   )
 }
